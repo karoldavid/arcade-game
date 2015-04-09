a423c99b94a2d66667980ea499998f33019e8d95
lorak <k.zysk@zoho.com>
2015-04-09 20:44:14 +0200

    add score and choose player

--- js/app.js
+++ js/app.js
@@ -6,27 +6,13 @@ function getRandomHeight(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-// Enemies our player must avoid
 var Enemy = function() {
-    // Variables applied to each of our instances go here,
-    // we've provided one for you to get started
-
-    // The image/sprite for our enemies, this uses
-    // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
     this.x = -50;
     this.y = getRandomHeight(50,250);
     this.speed = getRandomSpeed(20,60);
 }
 
-Enemy.prototype.reset = function() {
-  this.x = -50;
-  this.y = getRandomHeight(50,250);
-  this.speed = getRandomSpeed(20,60);
-}
-
-// Update the enemy's position, required method for game
-// Parameter: dt, a time delta between ticks
 Enemy.prototype.update = function(dt) {
   if (this.x <= 550) {
     this.x = this.x + (this.speed * dt);
@@ -34,21 +20,13 @@ Enemy.prototype.update = function(dt) {
     this.reset();
   }
   if (this.detectCollission()) {
-    player.reset();
+    player.reset(true);
   }
-    // You should multiply any movement by the dt parameter
-    // which will ensure the game runs at the same speed for
-    // all computers.
 }
 
-// Draw the enemy on the screen, required method for game
 Enemy.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 }
-
-// Now write your own player class
-// This class requires an update(), render() and
-// a handleInput() method.
 
 Enemy.prototype.detectCollission = function() {
   var enemyBox = { 'x' : 50, 'y' : 50 },
@@ -62,12 +40,70 @@ Enemy.prototype.detectCollission = function() {
             e.bottom < p.top);
 }
 
-var Player = function() {
-  this.sprite = 'images/char-boy.png';
+Enemy.prototype.reset = function() {
+  this.x = -50;
+  this.y = getRandomHeight(50,250);
+  this.speed = getRandomSpeed(20,60);
+}
+
+var Player = function(chosenPlayer) {
+  this.sprite = chosenPlayer;
   this.x = 200;
   this.y = 400;
   this.speed = 1;
   this.move = { 'x' : 0, 'y' : 0 };
+  this.score = 0;
+}
+
+function choosePlayer() {
+    var canvas = document.getElementsByTagName("canvas")[0],
+        playerImage = ['boy', 'cat-girl', 'horn-girl', 'pink-girl', 'princess-girl'],
+        imageUrl,
+        image = 0,
+        imageObj = new Image();
+
+        ctx.font = '35pt Calibri';
+        ctx.fillStyle = 'black';
+        ctx.fillText("Classic Arcade Game",  canvas.width / 2 - 200, canvas.height / 2 - 50);
+
+        ctx.font = '20pt Calibri';
+        ctx.fillStyle = 'red';
+        ctx.fillText("Press Enter to Choose Player",  canvas.width / 2 - 150, canvas.height / 2 - 10);
+
+        function changeImage() {
+          imageUrl = "images/char-" + playerImage[image] + ".png";
+          imageObj.src = imageUrl;
+          ctx.drawImage(imageObj, canvas.width / 2 - 50, canvas.height / 2);
+          image = image === 4 ? 0 : image + 1;
+          //setTimeout(changeImage, 2000);
+          return imageUrl;
+        }
+    return changeImage();
+}
+
+function chosenPlayer(currentPlayer) {
+  var playerImage = ['boy', 'cat-girl', 'horn-girl', 'pink-girl', 'princess-girl'],
+      imageUrl;
+    var length = playerImage.length;
+    for (var i = 0; i < length; i++) {
+      console.log(playerImage[i]);
+        if (currentPlayer.match(playerImage[i]) && i + 1 < length) return "images/char-" + playerImage[i+1] + ".png";
+    }
+    return "images/char-" + playerImage[0] + ".png";
+}
+
+
+Player.prototype.update = function() {
+  this.inBounds();
+  if (this.detectGoal()) {
+    this.score += 10;
+    this.reset(false);
+  }
+  this.updateScore();
+}
+
+Player.prototype.render = function() {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 }
 
 Player.prototype.inBounds = function() {
@@ -80,19 +116,28 @@ Player.prototype.inBounds = function() {
   if (newY >= -10 && newY <= 400) {
     this.y = newY;
   }
-  // this.move = { 'x' : 0, 'y' : 0 };
 }
 
-Player.prototype.update = function() {
-  this.inBounds();
+Player.prototype.detectGoal = function() {
+  return !( this.y >= 0);
+}
 
-  if (this.detectGoal()) {
-    this.reset();
+Player.prototype.reset = function(collision) {
+  this.x = 200;
+  this.y = 400;
+  this.speed = 1;
+  this.move = { 'x' : 0, 'y' : 0 };
+  if (collision) {
+    this.score = 0;
   }
 }
 
-Player.prototype.render = function() {
-    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+Player.prototype.updateScore = function() {
+  var canvas = document.getElementsByTagName("canvas")[0];
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  ctx.font = '20pt Calibri';
+  ctx.fillStyle = 'red';
+  ctx.fillText("Score: " + this.score, 0, 605);
 }
 
 Player.prototype.handleInput = function(key) {
@@ -112,24 +157,6 @@ Player.prototype.handleInput = function(key) {
     }
 }
 
-Player.prototype.detectGoal = function() {
-  return !( this.y >= 0);
-}
-
-Player.prototype.reset = function() {
-  this.x = 200;
-  this.y = 400;
-  this.speed = 1;
-  this.move = { 'x' : 0, 'y' : 0 };
-}
-
-
-// Now instantiate your objects.
-// Place all enemy objects in an array called allEnemies
-// Place the player object in a variable called player
-
-// This listens for key presses and sends the keys to your
-// Player.handleInput() method. You don't need to modify this.
 document.addEventListener('keyup', function(e) {
     var allowedKeys = {
         37: 'left',
@@ -137,5 +164,11 @@ document.addEventListener('keyup', function(e) {
         39: 'right',
         40: 'down'
     };
+    var playerKey = {
+      13: 'enter'
+    }
+    if (playerKey[e.keyCode]) {
+      player.sprite = chosenPlayer(player.sprite);
+    }
     player.handleInput(allowedKeys[e.keyCode]);
 });
